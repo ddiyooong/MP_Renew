eb677d8e77b8499ab19d32678af5ae00d5adcb2d
ddiyooong <104419757+ddiyooong@users.noreply.github.com>
2023-02-27 01:57:39 +0900

    new File  & Packaging

---
+++ app/src/main/java/com/example/wheelro/activity/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wheelro.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wheelro.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
